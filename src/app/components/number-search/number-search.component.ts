39fa0b1d533b5893b67e3a723e862c5ab6fbb078
Francois Joubert <francois.joubert@opsi.co.za>
2020-10-27 13:32:11 +0200

    Added Q4 - Numbers search

---
+++ src/app/components/number-search/number-search.component.ts
@@ -0,0 +1,46 @@
+import { Component } from "@angular/core";
+import { NumberSearchService } from "../../services";
+import { numbersTestList } from "../../data/numbers";
+
+@Component({
+  selector: "app-number-search-tester",
+  templateUrl: "./number-search.component.html",
+  styleUrls: ["./number-search.component.css"]
+})
+export class NumberSearchComponent {
+  public tests: {
+    label: string;
+    value: any;
+    error?: string;
+    expected: any;
+    pass: boolean;
+  }[];
+
+  constructor(private _numberSearchService: NumberSearchService) {
+    this.tests = numbersTestList.map(testCase => this.test(testCase));
+  }
+
+  public test(testCase: any) {
+    try {
+      let closestPair = this._numberSearchService.findClosestPair(
+        testCase.value
+      );
+
+      return {
+        label: testCase.label,
+        value: closestPair,
+        expected: testCase.res,
+        pass:
+          closestPair[0] == testCase.res[0] && closestPair[1] == testCase.res[1]
+      };
+    } catch (e) {
+      return {
+        label: testCase.label,
+        value: undefined,
+        error: e.message,
+        expected: testCase.res,
+        pass: false
+      };
+    }
+  }
+}
---
+++ src/app/services/number-search.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from "@angular/core";
+
+@Injectable()
+export class NumberSearchService {
+  public findClosestPair(numbers: number[]): [number, number] {
+    throw Error("Not implemented");
+  }
+}
