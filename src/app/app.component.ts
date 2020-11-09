4dcc1e017d5adcf54fb7dbf1c10b1646f0cda26d
francois-joubert <francois.joubert@opsi.co.za>
2020-11-09 08:46:57 +0200

    Added switch for app.component
    (cherry picked from commit de1adf5525cbcf31592d4669efc64c83a3c5b734)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,6 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: "app",
-  //template: "<app-list></app-list>"
-  template: "<app-numbers></app-numbers>"
-  //template: "<app-palindrome-tester></app-palindrome-tester>"
-  //template: "<app-number-search-tester></app-number-search-tester>"
+  templateUrl: "app.component.html"
 })
 export class AppComponent {}
