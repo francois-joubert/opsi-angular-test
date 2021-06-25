5263f5ef426de10ad4f0f393037468968b932ed8
Francois Joubert <francois.joubert@opsi.co.za>
2021-06-25 11:31:57 +0200

    PROJ: Update project for VS.

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,14 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
-platformBrowserDynamic().bootstrapModule(AppModule).then(ref => {
+platformBrowserDynamic().bootstrapModule(AppModule).then(ref =>
+{
   // Ensure Angular destroys itself on hot reloads.
-  if (window['ngRef']) {
-    window['ngRef'].destroy();
+  if ((window as any)['ngRef'])
+  {
+    (window as any)['ngRef'].destroy();
   }
-  window['ngRef'] = ref;
+  (window as any)['ngRef'] = ref;
 
   // Otherwise, log the boot error
 }).catch(err => console.error(err));
